a3616ec2277bc475ea93a2b24e7b2c9e8b4641de
Edward Fox <edward.fox@epigenesys.org.uk>
2020-01-13 16:10:10 +0000

    Updated the Rules section Updated the React component so that it works

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,11 @@ require("trix");
 require("@rails/actiontext");
 require("select2");
 
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
+
 import 'bootstrap';
 
 Rails.start();
