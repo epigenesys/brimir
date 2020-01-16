1c8b201dc758f0f0c28897d0bc13133bc1963e35
Edward Fox <edward.fox@epigenesys.org.uk>
2020-01-16 14:23:43 +0000

    Commented out the original JavaScript code

--- app/assets/javascripts/tickets.js
+++ app/assets/javascripts/tickets.js
@@ -1,90 +1,90 @@
-// Brimir is a helpdesk system to handle email support requests.
-// Copyright (C) 2012-2015 Ivaldi http://ivaldi.nl
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// // Brimir is a helpdesk system to handle email support requests.
+// // Copyright (C) 2012-2015 Ivaldi http://ivaldi.nl
+// //
+// // This program is free software: you can redistribute it and/or modify
+// // it under the terms of the GNU Affero General Public License as published by
+// // the Free Software Foundation, either version 3 of the License, or
+// // (at your option) any later version.
+// //
+// // This program is distributed in the hope that it will be useful,
+// // but WITHOUT ANY WARRANTY; without even the implied warranty of
+// // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// // GNU Affero General Public License for more details.
+// //
+// // You should have received a copy of the GNU Affero General Public License
+// // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-jQuery(function() {
+// jQuery(function() {
 
-  // TODO #1 - Make ajax modal style
-  jQuery('[data-assignee-id]').click(function(e) {
-    e.preventDefault();
+//   // TODO #1 - Make ajax modal style
+//   jQuery('[data-assignee-id]').click(function(e) {
+//     e.preventDefault();
 
-    var elem = jQuery(this);
-    var dialog = jQuery('#change-assignee');
-    var options = dialog.find('form select');
+//     var elem = jQuery(this);
+//     var dialog = jQuery('#change-assignee');
+//     var options = dialog.find('form select');
 
-    /* set ticket id */
-    dialog.find('form').attr('action',
-        elem.parents('[data-ticket-url]').data('ticket-url'));
+//     /* set ticket id */
+//     dialog.find('form').attr('action',
+//         elem.parents('[data-ticket-url]').data('ticket-url'));
 
-    /* select assigned user */
-    options.removeAttr('selected');
-    options.find('[value="' + elem.data('assignee-id') + '"]').attr('selected', 'selected');
+//     /* select assigned user */
+//     options.removeAttr('selected');
+//     options.find('[value="' + elem.data('assignee-id') + '"]').attr('selected', 'selected');
 
-      /* show the dialog */
-    dialog.foundation('reveal','open');
+//       /* show the dialog */
+//     dialog.foundation('reveal','open');
 
-  });
+//   });
 
-  // TODO #2 - Do we need the add label button to be a modal anymore?
-  jQuery('.select2-create').select2({
-    width: 'resolve',
-    createSearchChoicePosition: 'bottom',
-    createSearchChoice: function(term, data) {
-      return { id:term, text:term };
-    },
-    ajax: {
-      url: '/labels.json',
-      dataType: 'json',
-      data: function (term, page) {
-        return {
-          q: term
-        };
-      },
-      results: function (data) {
-        return { results: data };
-      }
-    }
-  });
+//   // TODO #2 - Do we need the add label button to be a modal anymore?
+//   jQuery('.select2-create').select2({
+//     width: 'resolve',
+//     createSearchChoicePosition: 'bottom',
+//     createSearchChoice: function(term, data) {
+//       return { id:term, text:term };
+//     },
+//     ajax: {
+//       url: '/labels.json',
+//       dataType: 'json',
+//       data: function (term, page) {
+//         return {
+//           q: term
+//         };
+//       },
+//       results: function (data) {
+//         return { results: data };
+//       }
+//     }
+//   });
 
-  // TODO #3 - Explore this, how do canned replies work
-  jQuery('#canned-reply').on('change', function(){
-    var editor = jQuery(this).parents('form#new_reply').find('trix-editor')[0].editor;
-    var url = this.value;
-    if (url) {
-      jQuery.ajax({
-        url: url
-      }).done(function(response) {
-        editor.loadHTML(response.message);
-      });
-    } else {
-      editor.loadHTML('');
-    }
-  });
+//   // TODO #3 - Explore this, how do canned replies work
+//   jQuery('#canned-reply').on('change', function(){
+//     var editor = jQuery(this).parents('form#new_reply').find('trix-editor')[0].editor;
+//     var url = this.value;
+//     if (url) {
+//       jQuery.ajax({
+//         url: url
+//       }).done(function(response) {
+//         editor.loadHTML(response.message);
+//       });
+//     } else {
+//       editor.loadHTML('');
+//     }
+//   });
 
-  // TODO #4 - Update locking, maybe move it to a websocket connection. Has a
-  //           higher upfront cost, but no need for re-hitting endpoint)
-  if(jQuery('[data-lock-path]').length > 0) {
+//   // TODO #4 - Update locking, maybe move it to a websocket connection. Has a
+//   //           higher upfront cost, but no need for re-hitting endpoint)
+//   if(jQuery('[data-lock-path]').length > 0) {
 
-    function keepLock() {
-      jQuery.ajax({
-        url: jQuery('[data-lock-path]').data('lock-path'),
-        type: 'post'
-      });
-    }
-    keepLock();
-    /* renew lock every 4 minutes */
-    setInterval(keepLock, 1000 * 60 * 4);
-  }
-});
+//     function keepLock() {
+//       jQuery.ajax({
+//         url: jQuery('[data-lock-path]').data('lock-path'),
+//         type: 'post'
+//       });
+//     }
+//     keepLock();
+//     /* renew lock every 4 minutes */
+//     setInterval(keepLock, 1000 * 60 * 4);
+//   }
+// });
