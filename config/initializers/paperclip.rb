c459171d4e6db34dc29ed95b3dbeafcfec9e637a
James Gregory-Monk <james.gregory@epigenesys.org.uk>
2022-03-30 10:23:22 +0100

    Deactivate Paperclip spoof detection (seems to be broken/unreliable)

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,13 @@
 Paperclip.options[:content_type_mappings] = {
   swm: %w(application/zip)
 }
+
+# Source: https://github.com/thoughtbot/paperclip/issues/1429#issuecomment-34390771
+require 'paperclip/media_type_spoof_detector'
+module Paperclip
+  class MediaTypeSpoofDetector
+    def spoofed?
+      false
+    end
+  end
+end
